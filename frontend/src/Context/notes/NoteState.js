c22bf185b540700e1d98aefa3fe250919b1ea377
[princeyadav09] <[princekumaryadav09@gmail.com]>
2023-05-27 10:11:57 +0530

    frontend

--- frontend/src/Context/notes/NoteState.js
+++ frontend/src/Context/notes/NoteState.js
@@ -3,61 +3,107 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState =(props)=>{
+    const host = "http://localhost:4000"
 
-    const notesInitial = [
-            {
-              "_id": "646e40a8847760a92491cb79",
-              "user": "646c7955df2949788cde0027",
-              "title": "My Title",
-              "description": "Please wake up early",
-              "tag": "personal",
-              "date": "2023-05-24T16:51:52.755Z",
-              "__v": 0
-            },
-            {
-              "_id": "646f10f5f669703826933f01",
-              "user": "646c7955df2949788cde0027",
-              "title": "My Title",
-              "description": "Please wake up early",
-              "tag": "personal",
-              "date": "2023-05-25T07:40:37.686Z",
-              "__v": 0
-            },
-            {
-              "_id": "646f10f9f669703826933f03",
-              "user": "646c7955df2949788cde0027",
-              "title": "My Title",
-              "description": "Please wake up early",
-              "tag": "personal",
-              "date": "2023-05-25T07:40:41.221Z",
-              "__v": 0
-            },
-            {
-              "_id": "646f10f5f669703826933f01",
-              "user": "646c7955df2949788cde0027",
-              "title": "My Title",
-              "description": "Please wake up early",
-              "tag": "personal",
-              "date": "2023-05-25T07:40:37.686Z",
-              "__v": 0
-            },
-            {
-              "_id": "646f10f9f669703826933f03",
-              "user": "646c7955df2949788cde0027",
-              "title": "My Title",
-              "description": "Please wake up early",
-              "tag": "personal",
-              "date": "2023-05-25T07:40:41.221Z",
-              "__v": 0
+    const notesInitial = []
+
+
+    const [notes,setNotes] = useState(notesInitial)
+
+       // Get all note
+
+       const getNotes = async ()=>{
+        //TODO api call
+
+        const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ2Yzc5NTVkZjI5NDk3ODhjZGUwMDI3In0sImlhdCI6MTY4NDgzNzU1MH0.fqRwiKNuyKvkfXEh6bDrud41uvrV7GYSy3mIpxrnytA"
             }
-          ]
+        });
 
-          const [notes,setNotes] = useState(notesInitial)
+        const json = await response.json()
+        console.log(json)
+        setNotes(json)
+
+      }
+
+          // Add a note
+
+          const addNote = async (title,description,tag)=>{
+            //TODO api call
+
+            const response = await fetch(`${host}/api/notes/addnote`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ2Yzc5NTVkZjI5NDk3ODhjZGUwMDI3In0sImlhdCI6MTY4NDgzNzU1MH0.fqRwiKNuyKvkfXEh6bDrud41uvrV7GYSy3mIpxrnytA"
+                },
+                body:JSON.stringify({title,description,tag})
+            });
+ 
+            const note = await response.json();
+            setNotes(notes.concat(note))
+                
+          }
+
+          // Delete a note
+
+          const deleteNote = async (id,title,description,tag)=>{
+            //Api Call
+            const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ2Yzc5NTVkZjI5NDk3ODhjZGUwMDI3In0sImlhdCI6MTY4NDgzNzU1MH0.fqRwiKNuyKvkfXEh6bDrud41uvrV7GYSy3mIpxrnytA"
+                },
+                body:JSON.stringify({title,description,tag})
+            });
+            const json = await response.json();
+            console.log(json);
+
+            const newNotes = notes.filter((note)=>{return note._id!==id})
+            setNotes(newNotes)
+          }
+
+          // Edit a note
+
+          const editNote = async (id, title,description,tag)=>{
+            // Api call
+
+            const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ2Yzc5NTVkZjI5NDk3ODhjZGUwMDI3In0sImlhdCI6MTY4NDgzNzU1MH0.fqRwiKNuyKvkfXEh6bDrud41uvrV7GYSy3mIpxrnytA"
+                },
+                body:JSON.stringify({title,description,tag})
+            });
+            const json = await response.json();
+            console.log(json);
+
+            let newNotes = JSON.parse(JSON.stringify(notes))
+
+            // Logic to edit in client
+                for (let index = 0; index < newNotes.length; index++) {
+                    const element = newNotes[index];
+                    if(element._id === id){
+                        newNotes[index].title = title;
+                        newNotes[index].description = description;
+                        newNotes[index].tag = tag;
+                        break;
+                    }
+               
+                }
+                console.log(notes);
+                setNotes(newNotes);
+          }
    
    
     return (
         
-        <NoteContext.Provider value={{notes,setNotes}}>
+        <NoteContext.Provider value={{notes,addNote,deleteNote,editNote,getNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,9 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import{ Link, useLocation } from "react-router-dom";
 
 const Navbar =()=>{
     let location = useLocation();
-    useEffect(()=>{
-        console.log(location.pathname);
-    },[location]);
+
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
    <div className="container-fluid">
@@ -16,10 +14,10 @@ const Navbar =()=>{
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className={'nav-link ${location.pathname==="/"? "active": ""}'} aria-current="page" to="/">Home</Link>
+          <Link className={`nav-link ${location.pathname==="/"? "active": ""}`} aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className={'nav-link ${location.pathname==="/about"? "active": ""}'} to="/about">About</Link>
+          <Link className={`nav-link ${location.pathname==="/about"? "active": ""}`} to="/about">About</Link>
         </li>
         
       </ul>
