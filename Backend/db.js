8c79d894ad6ce4564e68feb57890c99c8cadf3a1
[princeyadav09] <[princekumaryadav09@gmail.com]>
2023-07-09 22:54:43 +0530

    final

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/inotebook?directConnection=true"
+const mongoURI = "mongodb+srv://princeyadav09:yadav@cluster0.ozvdgx9.mongodb.net/"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI)
-    .then(() => {
-      console.log('Connected to MongoDB');
+    .then((res) => {
+      console.log('Connected to MongoDB',res.connection.host);
     })
     .catch((error) => {
       console.error(error);
